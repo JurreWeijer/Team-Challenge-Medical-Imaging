bfadc13f3442a8c2b533a5f57a9f3297cc22b4f5
StijnvWijn <s.a.v.wijngaarden@student.tue.nl>
2023-02-10 12:55:13 +0100

    Finished simple segmentation algorithm

--- Segmentation.py
+++ Segmentation.py
@@ -29,42 +29,75 @@ OpeningFilter = sitk.BinaryMorphologicalOpeningImageFilter()
 ClosingFilter = sitk.BinaryMorphologicalClosingImageFilter()
 ThinningFilter = sitk.BinaryThinningImageFilter()
 
+def SimpleSegmentation(image):
+    threshold = 150
+    OpeningSize = 1
+    ClosingSize = 2
+
+    ThresholdFilter.SetLowerThreshold(threshold)
+    ThresholdFilter.SetUpperThreshold(int(np.max(image_array)))
+    OpeningFilter.SetKernelRadius(OpeningSize)
+    ClosingFilter.SetKernelRadius(ClosingSize)
+
+    print("Starting SimpleSegmentation sequence")
+
+    img_processed = ThresholdFilter.Execute(image)
+
+    print("Thresholding completed with a threshold of " + str(threshold))
+
+    img_processed = OpeningFilter.Execute(img_processed)
+
+    print("Binary Morphological opening completed with a kernel size of " + str(OpeningSize))
+
+    img_processed = ClosingFilter.Execute(img_processed)
+
+    print("Binary Morphological closing completed with a kernel size of " + str(ClosingSize))
+
+    print("SimpleSegmentation done")
+
+    return img_processed
+
+def BanikSegmentation(image):
+# Segmentation based on the paper by Banik et al.
+# Automatic Segmentation of the Ribs, the Vertebral Column, and the Spinal Canal in Pediatric Computed Tomographic Images  https://doi.org/10.1007/s10278-009-9176-x
+
+# Steps described:
+# RIBS:
+# Binarize by thresholding @ 200 HU
+# Morphologically open with radius of 3 pixels
+# Find central line along the medial sagittal plane based on the inner contour of fat
+# Measure Euclidian Distance between:
+#   (a) Edge of region and inner contour of fat
+#   (b) Centroid of region and inner contour of fat
+#   (c) Edge of region and central line
+#   (d) Centroid of region and central line
+# Divide CT image in Upper and lower area:
+#   Lower area
+#   Keep region if a <= 3.5 cm AND b <= 1.8 cm AND c >= 1.5cm AND d >=1.5cm
+#   Upper area
+#   Keep region if a <= 4.5 cm AND b <= 3 cm AND c >= 1cm AND d >=1cm
+
+    ThresholdFilter.SetLowerThreshold(150) #Original paper gave 100, but this gives better results
+    ThresholdFilter.SetUpperThreshold(int(np.max(image_array)))
+    OpeningFilter.SetKernelRadius(1) #Original paper gave 3, but this gives better results
+    ClosingFilter.SetKernelRadius(2)
+
+    img_processed = ThresholdFilter.Execute(image)
+
+    img_processed = OpeningFilter.Execute(img_processed)
+
+
+
+    return img_processed
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     img_scoliosis = OpenScoliosis(scoliosis_path, "1preop.nii")
     image_array = sitk.GetArrayFromImage(img_scoliosis) #Image array has values based on Houndsfield units
-    print("Maximal values " + str(np.max(image_array)) + " Minimal values " + str(np.min(image_array)))
+    print("Maximal value " + str(np.max(image_array)) + " Minimal value " + str(np.min(image_array)))
 
-    #Segmentation based on the paper by Banik et al.
-    #Automatic Segmentation of the Ribs, the Vertebral Column, and the Spinal Canal in Pediatric Computed Tomographic Images  https://doi.org/10.1007/s10278-009-9176-x
-
-    #Steps described:
-    # RIBS:
-    # Binarize by thresholding @ 200 HU
-    # Morphologically open with radius of 3 pixels
-    # Find central line along the medial sagittal plane based on the inner contour of fat
-    # Measure Euclidian Distance between:
-    #   (a) Edge of region and inner contour of fat
-    #   (b) Centroid of region and inner contour of fat
-    #   (c) Edge of region and central line
-    #   (d) Centroid of region and central line
-    # Divide CT image in Upper and lower area:
-    #   Lower area
-    #   Keep region if a <= 3.5 cm AND b <= 1.8 cm AND c >= 1.5cm AND d >=1.5cm
-    #   Upper area
-    #   Keep region if a <= 4.5 cm AND b <= 3 cm AND c >= 1cm AND d >=1cm
-
-    ThresholdFilter.SetLowerThreshold(-10) #Original paper gave 100, but this gives better results
-    ThresholdFilter.SetUpperThreshold(int(np.max(image_array)))
-    OpeningFilter.SetKernelRadius(3)
-
-    img_processed = ThresholdFilter.Execute(img_scoliosis)
-
-    img_processed = OpeningFilter.Execute(img_processed)
-
-
+    img_processed = SimpleSegmentation(img_scoliosis)
 
     VoxelSize = img_scoliosis.GetSpacing()
     segmentation_array = sitk.GetArrayFromImage(img_processed)
@@ -73,7 +106,7 @@ if __name__ == '__main__':
     plt.imshow(image_array[slice_num, :, :], cmap="gray")
     plt.title("Original Image")
     plt.figure()
-    plt.imshow(segmentation_array[slice_num,:,:], cmap="gray")
+    plt.imshow(image_array[slice_num, :, :]*segmentation_array[slice_num,:,:], cmap="gray")
     plt.title("Segmented Image")
     plt.show()
 
